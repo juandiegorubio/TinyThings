bc3db96428eb905a65a94ddb06b4dba40c1deafc
juandiegorubio <juandiegors510@gmail.com>
2020-07-02 18:22:13 +0200

    Cosas tontas arregladas

--- CPPHelp/printer.cpp
+++ CPPHelp/printer.cpp
@@ -1,12 +1,15 @@
-#include <boost>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <list>
 #include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <boost/variant.hpp>
 
 
-typedef boost::variant<int, float, double, char, string> myvariant;
+typedef boost::variant<int, float, double, char, std::string> myvariant;
 
 class Printer {
 
@@ -24,27 +27,11 @@ public:
     cerrar = cer;
   }
 
-  void print(int salida) {
+  void print(myvariant salida) {
     std::cout << salida;
   }
 
-  void print(float salida) {
-    std::cout << salida;
-  }
-
-  void print(double salida) {
-    std::cout << salida;
-  }
-
-  void print(char salida) {
-    std::cout << salida;
-  }
-
-  void print(string salida) {
-    std::cout << salida;
-  }
-
-  void print(vector<myvariant> salida) {
+  void print(std::vector<myvariant> salida) {
     std::cout << abrir;
     for(int i = 0; i < salida.size()-1; ++i) {
       print(salida[i]);
@@ -54,7 +41,7 @@ public:
     std::cout << cerrar;
   }
 
-  void print(list<myvariant> salida) {
+  void print(std::list<myvariant> salida) {
     std::cout << abrir;
     auto itend = salida.end(); itend--;
     for(auto it = salida.begin(); it != itend; ++it){
@@ -65,5 +52,44 @@ public:
     std::cout << cerrar;
   }
 
+  void print(std::set<myvariant> salida) {
+    std::cout << abrir;
+    auto itend = salida.end(); itend--;
+    for(auto it = salida.begin(); it != itend; ++it){
+      print(*it);
+      std::cout << separador;
+    }
+    print(*itend);
+    std::cout << cerrar;
+  }
+
+  void print(std::unordered_set<myvariant> salida) {
+    std::cout << abrir;
+    auto itend = salida.end(); itend--;
+    for(auto it = salida.begin(); it != itend; ++it){
+      print(*it);
+      std::cout << separador;
+    }
+    print(*itend);
+    std::cout << cerrar;
+  }
+
+
+  void print(std::unordered_map<myvariant, myvariant> salida) {
+    std::cout << abrir;
+    auto itend = salida.end(); itend--;
+    for(auto it = salida.begin(); it != itend; ++it){
+      std::cout << abrir;
+      print(it->first);
+      std::cout << separador;
+      print(it->second);
+      std::cout << cerrar << ' ';
+    }
+    std::cout << abrir;
+    print(itend->first);
+    std::cout << separador;
+    print(itend->second);
+    std::cout << cerrar << cerrar;
+  }
 
 };
